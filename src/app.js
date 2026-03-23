dae618231fbe6e589bb2b2cae6c2283d500f9f57
Dheeraj Raja <dheerajraja111@gmail.com>
2026-03-24 00:11:46 +0530

    Auth module added

--- src/app.js
+++ src/app.js
@@ -2,19 +2,103 @@ const express = require('express');
 const connectDB = require('./config/database');
 const app = express();
 const User = require('./models/user');
+const { validateSignUpData } = require('./utils/validation');
+const bcrypt = require('bcrypt');
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
+const {userAuth} = require('./middlewares/auth');
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.post('/signup', async (req, res) => {
-    const user = new User(req.body);
-
+    // Validate the incoming data
     try {
+        validateSignUpData(req);
+
+        const { firstName, lastName, emailId, password } = req.body;
+
+        // Encrypt the password
+        const passwordHash = await bcrypt.hashSync(password, 10);
+
+        const user = new User({
+            firstName,
+            lastName,
+            emailId,
+            password: passwordHash
+        });
+    
         await user.save();
         res.send('User created successfully');
     } catch(err) {
         res.status(400).send('Error saving the user:' + err.message)
     }
 })
+
+app.post('/login', async (req, res) => {
+    try {
+        const { emailId, password } = req.body;
+
+        const user = await User.findOne({ emailId: emailId });
+
+        if (!user) {
+            throw new Error('Email Id is not present in the database');
+        }
+
+        const isPasswordValid = await user.validatePassword(password);
+        if (isPasswordValid) {
+            // Create a JWT token
+            const token = await user.getJWT();
+
+            // Add the token to cookie and send the response back to the user
+            res.cookie('token', token);
+            
+            res.send('Login successful');
+        } else {
+            throw new Error('Invalid password');
+        }
+    } catch (err) {
+        res.status(400).send('Error logging in:' + err.message);
+    }
+    
+});
+
+app.get('/profile', userAuth, async (req, res) => {
+
+    try {
+        const cookies = req.cookies;
+
+        const { token } = cookies;
+
+        if (!token) {
+            throw new Error('Invalid token');
+        }
+
+        // Validate my token
+        const decodedMessage = await jwt.verify(token, 'Mock@Tinder123');
+        
+        const { _id } = decodedMessage;
+        
+        const user = await User.findById(_id);
+        if (!user) {
+            throw new Error('User not found');
+        }
+
+        res.send(user);
+    } catch (err) {
+        res.status(400).send('Error fetching profile:' + err.message);
+    }
+});
+
+app.post('/sendConnectionRequest', userAuth, async (req, res) => {
+    try {
+        const user = req.user;
+        // Sending a connection request
+        res.send(user.firstName + ' sent the connection request!!');
+    } catch (err) {
+        res.status(400).send('Error sending connection request:' + err.message);
+    }
+});
 
 // Get user by email
 app.get('/user', async (req, res) => {
@@ -73,7 +157,6 @@ app.patch('/user/:userId', async (req, res) => {
             returnDocument: 'after',
             runValidators: true
         });
-        console.log(user);
         res.send('User updated successfully');
     } catch (err) {
         res.status(400).send('Error updating user:' + err.message);
---
+++ src/utils/validation.js
@@ -0,0 +1,18 @@
+const validator = require('validator');
+
+const validateSignUpData = (req) => {
+
+    const { firstName, lastName,emailId, password } = req.body;
+
+    if (!firstName || !lastName) {
+        throw new Error('Name is not valid!');
+    } else if (!validator.isEmail(emailId)) {
+        throw new Error('Invalid email format!');
+    } else if (!validator.isStrongPassword(password)) {
+        throw new Error('Password is not strong enough!');
+    }
+}
+
+module.exports = {
+    validateSignUpData
+};
